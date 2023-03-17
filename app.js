a183b121cae2f1962daaff8d9b40c49a0bd37ba4
UssLeonidas2003 <99397357+UssLeonidas2003@users.noreply.github.com>
2023-03-17 14:36:25 +0530

    Updated app.js
    fixed Cors for hosted fullstack

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'. 'https://heroic-kulfi-aa2eb0.netlify.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE');
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
